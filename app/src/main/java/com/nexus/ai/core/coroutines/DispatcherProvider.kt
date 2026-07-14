cdace342338d505f1fec892d29188696d0058065
aloft26armada-ux <aloft26armada@gmail.com>
2026-07-14 20:06:32 +0800

    Create DispatcherProvider.kt

---
+++ app/src/main/java/com/nexus/ai/core/coroutines/DispatcherProvider.kt
@@ -0,0 +1,21 @@
+package com.nexus.ai.core.coroutines
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Inject
+import javax.inject.Singleton
+
+interface DispatcherProvider {
+    fun main(): CoroutineDispatcher
+    fun io(): CoroutineDispatcher
+    fun default(): CoroutineDispatcher
+    fun unconfined(): CoroutineDispatcher
+}
+
+@Singleton
+class NexusDispatchers @Inject constructor() : DispatcherProvider {
+    override fun main(): CoroutineDispatcher = Dispatchers.Main
+    override fun io(): CoroutineDispatcher = Dispatchers.IO
+    override fun default(): CoroutineDispatcher = Dispatchers.Default
+    override fun unconfined(): CoroutineDispatcher = Dispatchers.Unconfined
+}
