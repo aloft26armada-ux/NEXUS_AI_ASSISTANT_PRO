0ad81c859a0a49b36f079be96aa87bcacb94b1dc
aloft26armada-ux <aloft26armada@gmail.com>
2026-07-14 19:56:08 +0800

    build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,6 @@
+plugins {
+    alias(libs.plugins.android.application) version "8.4.1" apply false
+    alias(libs.plugins.kotlin.android) version "1.9.24" apply false
+    alias(libs.plugins.hilt.android) version "2.51.1" apply false
+    alias(libs.plugins.ksp) version "1.9.24-1.0.20" apply false
+}
