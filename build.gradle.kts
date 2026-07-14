26545f13c5a086a5c0232cff7b54a65b21fa3209
aloft26armada-ux <aloft26armada@gmail.com>
2026-07-15 01:30:55 +0800

    Updated build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
-    alias(libs.plugins.android.application) version "8.4.1" apply false
-    alias(libs.plugins.kotlin.android) version "1.9.24" apply false
-    alias(libs.plugins.hilt.android) version "2.51.1" apply false
-    alias(libs.plugins.ksp) version "1.9.24-1.0.20" apply false
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
+    id("com.google.dagger.hilt.android") version "2.50" apply false
 }
-
